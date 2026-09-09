21fa988f7aa321c9e9782cc1d34ef6447c8802bf
Joseph Luker <JPLuker@pm.me>
2026-09-09 12:02:58 -0400

    Expand share config regression coverage for v0.9.0

--- tests/share-config.test.js
+++ tests/share-config.test.js
@@ -42,24 +42,39 @@ const token = ShareConfig.encode(roundTripSource);
 assert.match(token, /^[A-Za-z0-9_-]+$/);
 assert.deepEqual(ShareConfig.decode(token), roundTripSource);
 
+const zeroSelection = ShareConfig.create({ selectedRepos: [], modules: { repos: true } });
+assert.deepEqual(ShareConfig.decode(ShareConfig.encode(zeroSelection)), zeroSelection);
+
+const noModules = ShareConfig.create({ modules: { profile: false, stats: false, calendar: false, repos: false } });
+assert.deepEqual(ShareConfig.decode(ShareConfig.encode(noModules)), noModules);
+
 const legacyToken = Buffer.from(JSON.stringify(roundTripSource), 'utf8').toString('base64url');
 assert.deepEqual(ShareConfig.decode(legacyToken), roundTripSource);
 assert.ok(token.length < legacyToken.length, 'compact share tokens should be shorter than legacy JSON tokens');
 
 assert.equal(ShareConfig.tryDecode('not valid!'), null);
 assert.equal(ShareConfig.tryDecode(''), null);
+assert.equal(ShareConfig.tryDecode('a'.repeat(ShareConfig.MAX_TOKEN_LENGTH + 1)), null);
 
 const unsupported = Buffer.from(JSON.stringify({ v: 2 }), 'utf8').toString('base64url');
-assert.throws(
-  () => ShareConfig.decode(unsupported),
-  /Unsupported Gitbrag share config version: 2/
-);
+assert.throws(() => ShareConfig.decode(unsupported), /Unsupported Gitbrag share config version: 2/);
 
 const unsupportedCompact = Buffer.from(JSON.stringify([2, 15, 0, 0, [], 0, 0, 0]), 'utf8').toString('base64url');
-assert.throws(
-  () => ShareConfig.decode(unsupportedCompact),
-  /Unsupported Gitbrag share config version: 2/
-);
+assert.throws(() => ShareConfig.decode(unsupportedCompact), /Unsupported Gitbrag share config version: 2/);
+
+for (const malformed of [
+  [1, 15, 0, 0, []],
+  [1, 16, 0, 0, [], 0, 0, 0],
+  [1, 15, 99, 0, [], 0, 0, 0],
+  [1, 15, 0, 99, [], 0, 0, 0],
+  [1, 15, 0, 0, 'not-an-array', 0, 0, 0],
+  [1, 15, 0, 0, [], 99, 0, 0],
+  [1, 15, 0, 0, [], 0, 99, 0],
+  [1, 15, 0, 0, [], 0, 0, 99],
+]) {
+  const malformedToken = Buffer.from(JSON.stringify(malformed), 'utf8').toString('base64url');
+  assert.throws(() => ShareConfig.decode(malformedToken), /Invalid Gitbrag share config token/);
+}
 
 const copy = ShareConfig.defaults();
 copy.modules.profile = false;
